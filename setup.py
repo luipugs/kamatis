fa4da269023e2c423f8ea5877a19c316b52b0db2
Luis Manuel R. Pugoy <lpugoy@insynchq.com>
2016-09-20 02:32:36 +0800

    Add more information to setup script

--- setup.py
+++ setup.py
@@ -1,10 +1,38 @@
-from setuptools import setup, find_packages
+from setuptools import (
+    find_packages,
+    setup,
+    )
+import os
+import re
+
+
+with open(os.path.join('kamatis', '__init__.py'), 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
 
 
 def main():
     setup(
         name='kamatis',
+        version=version,
+        author='Luis Manuel R. Pugoy',
+        author_email='luipugs@gmail.com',
+        maintainer='Luis Manuel R. Pugoy',
+        maintainer_email='luipugs@gmail.com',
+        url='https://github.com/luipugs/kamatis/',
         description='Pomodoro timer',
+        classifiers=(
+            'Development Status :: 3 - Alpha',
+            'Environment :: X11 Applications :: Qt',
+            'Intended Audience :: End Users/Desktop',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 2.7',
+            ),
+        license='BSD',
         packages=find_packages('.'),
         entry_points={
             'console_scripts': [
